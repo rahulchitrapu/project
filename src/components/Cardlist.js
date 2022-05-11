b5c4a5036d4e5c2fe1f657e04fc3491780691294
rahulchitrapu <91407582+rahulchitrapu@users.noreply.github.com>
2022-05-11 17:04:34 +0530

    Update Cardlist.js

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -5,7 +5,7 @@ function Cardlist({posts}) {
     return (
         <div className='CardList' style={{display:"flex",flexDirection:"row",flexWrap:"wrap"}}>
             {posts.map((post)=>{
-                return <Card post={post}/>
+                 return <div key={post.id}><Card post={post}/></div>
             })}
         </div>
     );
