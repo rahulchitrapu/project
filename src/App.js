82873b9ea03b5254611f1c32e38b33b133beb0fe
rahulchitrapu <91407582+rahulchitrapu@users.noreply.github.com>
2022-05-11 17:18:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import Cardlist from './components/Cardlist';
 function App() {
   const [value,setValue]=useState('');
   const [post,setPost]=useState([]);
+  const [filtedArr,setFiltedArr]=useState([]);
   useEffect(()=>{
     fetch(`https://jsonplaceholder.typicode.com/posts`)
     .then(res=>res.json())
     .then(res=>{
-      setPost(res)})
+    setPost(res)
+    setFiltedArr(res)})
   },[])
+  
   const searchFunction=(search)=>{
-   setPost(post.filter((e)=>{
+   let arr=post.filter((e)=>{
       return e['title'].toLowerCase().includes(search.toLowerCase())
-    }))
+    })
+    setFiltedArr(arr)
+
     
   }
  if(post.length===0){
@@ -32,7 +37,7 @@ function App() {
         <input placeholder='search' value={value} onChange={(e)=>{setValue(e.target.value)}}/>
         <button style={{margin:"10px"}} onClick={()=>searchFunction(value)}>search</button>
       </div>
-      <div><Cardlist posts={post}/></div>
+      <div><Cardlist posts={filtedArr}/></div>
     </div>
   );
   
